39b68e4361f28a354bdd44aea9d3e9f9852d5f6d
tyronmaster <degonski.art@gmail.com>
2024-01-12 20:54:57 +0100

    feat: add simpliest custom slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     Circle = "circle",
     Rounded = "rounded",
   }
-  const [slides, setSlides] = useState([
+  const [slides] = useState([
     {
       id: 1,
       mask: MaskTypes.LeafLeft,
@@ -52,8 +52,46 @@ function App() {
       date: "22 ноября 2023      ",
     },
   ]);
+  const [trackStart, setTrackStart] = useState(0);
+  const [buttonsState, setButtonsState] = useState({
+    right: true,
+    left: false,
+  });
+  const sliderTrack = useRef(null);
+  const speed = 150;
+
+  function pressRight() {
+    const trackWidth = sliderTrack.current.offsetWidth;
+    const windowWidth = window.innerWidth;
+
+    if (trackStart + trackWidth - speed > windowWidth) {
+      if (!buttonsState.left)
+        setButtonsState({ ...buttonsState, left: true });
+
+      const start = trackStart - speed;
+      sliderTrack.current.style.left = start + "px";
+      setTrackStart(start);
+    } else {
+      const start = -(trackWidth - windowWidth);
+      sliderTrack.current.style.left = start + "px";
+      setTrackStart(start);
+      setButtonsState({ ...buttonsState, right: false });
+    }
+  }
+
+  function pressLeft() {
+    if (trackStart < 0) {
+      if (!buttonsState.right) setButtonsState({ ...buttonsState, right: true });
+      const start = trackStart + speed;
+      sliderTrack.current.style.left = start + "px";
+      setTrackStart(start);
+    } else {
+      setButtonsState({ ...buttonsState, left: false });
+    }
+  }
+
   return (
-    <>
+    <div className="slider">
       <div className="slider__header container">
         <h1 className="slider__title">Полезные материалы</h1>
         <div className="slider__description">
@@ -65,22 +103,36 @@ function App() {
 
       <div className="slider__wrapper">
         <div className="slider__track">
-          {slides.map((slide) => (
-            <div className="slider__item">
-              <div className={"item__picture" + " " + slide.mask}>
-                <img src={slide.src} alt="item image" className="item__image" />
+          <div className="slider__items" ref={sliderTrack}>
+            {slides.map((slide) => (
+              <div className="slider__item" key={slide.id}>
+                <div className={"item__picture" + " " + slide.mask}>
+                  <img
+                    src={slide.src}
+                    alt="item image"
+                    className="item__image"
+                  />
+                </div>
+                <div className="item__text">
+                  <a href="#" className="item__link">
+                    {slide.title}
+                  </a>
+                  <p className="item__date">{slide.date}</p>
+                </div>
               </div>
-              <div className="item__text">
-                <a href="#" className="item__link">
-                  {slide.title}
-                </a>
-                <p className="item__date">{slide.date}</p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-        <div className="slider__controls">
-          <div className="slider__control left">
+
+        <div className="slider__controls container">
+          <div
+            className={
+              buttonsState.left
+                ? "slider__control left"
+                : "slider__control left disabled"
+            }
+            onClick={pressLeft}
+          >
             <svg
               width="173"
               height="23"
@@ -92,16 +144,23 @@ function App() {
               <path
                 d="M12 0V0C12 6.07513 7.07513 11 0.999998 11L-9.69627e-07 11"
                 stroke="#7884A5"
-                stroke-width="3"
+                strokeWidth="3"
               />
               <path
                 d="M12 23V23C12 16.9249 7.07513 12 1 12L6.11999e-07 12"
                 stroke="#7884A5"
-                stroke-width="3"
+                strokeWidth="3"
               />
             </svg>
           </div>
-          <div className="slider__control right">
+          <div
+            className={
+              buttonsState.right
+                ? "slider__control right"
+                : "slider__control right disabled"
+            }
+            onClick={pressRight}
+          >
             <svg
               width="173"
               height="23"
@@ -118,18 +177,18 @@ function App() {
               <path
                 d="M161 0V0C161 6.07513 165.925 11 172 11L173 11"
                 stroke="#7884A5"
-                stroke-width="3"
+                strokeWidth="3"
               />
               <path
                 d="M161 23V23C161 16.9249 165.925 12 172 12L173 12"
                 stroke="#7884A5"
-                stroke-width="3"
+                strokeWidth="3"
               />
             </svg>{" "}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
